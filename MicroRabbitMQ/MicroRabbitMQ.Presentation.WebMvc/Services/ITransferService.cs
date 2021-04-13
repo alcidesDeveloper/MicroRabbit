743c50f90014631784eae3ca671ae449c2100e1f
alcides evangelista <alcides_44@yahoo.com>
2021-04-12 22:49:11 -0300

    Adicionando AspNet MVC

---
+++ MicroRabbitMQ/MicroRabbitMQ.Presentation.WebMvc/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbitMQ.Presentation.WebMvc.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMQ.Presentation.WebMvc.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+}
